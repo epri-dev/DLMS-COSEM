7a5f211bb38536cec529baf68899c155674fbb4e
Ed Beroset <beroset@ieee.org>
2016-09-22 10:54:34 -0400

    Added simple prototype code.
    This code is just a simple stub showing the use of CMake
    and asio with C++11.

---
+++ src/main.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <cstdlib>
+#include <string>
+#include <functional>
+#include <chrono>
+#include <asio.hpp>
+#include <asio/steady_timer.hpp>
+
+// this allows us to use string literals such as "5ms" or "1s"
+using namespace std::chrono_literals;
+
+/* simple count down class showing use of asio service
+ *
+ * This is based on a tutorial sample for an asynchronous timer
+ * as in the documentation for asio, but updated to use C++11
+ * constructs instead of boost.
+ */
+class CountDown
+{
+public:
+    CountDown(asio::io_service &io, int startvalue, std::string msg) :
+        timer_{io, 1s},
+        count_{startvalue},
+        message_{msg}
+    {
+        timer_.async_wait(std::bind(&CountDown::print, this));
+    }
+    ~CountDown() {
+        std::cout << message_ << "\n";
+    }
+    void print() {
+        if (count_) {
+            std::cout << count_ << std::endl;
+            --count_;
+            timer_.expires_from_now(1s);
+            timer_.async_wait(std::bind(&CountDown::print, this));
+        }
+    }
+    
+private:
+    asio::steady_timer timer_;
+    int count_;
+    std::string message_;
+};
+
+// simple test program
+int main(int argc, char *argv[])
+{
+    if (argc != 3) {
+        std::cout << "Usage: " << argv[0] << " countdownseconds message\n";
+        return 1;
+    }
+    asio::io_service io;
+    CountDown cd(io, std::atoi(argv[1]), argv[2]);
+    io.run();
+}
